555f81609020d6d84acccf083abeb41b202f4466
KazanNinja <thekazanninja@gmail.com>
2018-10-24 11:45:02 -0700

    Initial Commit. Adding time rounding system to fix potential rounding issues over long periods of time.

---
+++ Main.py
@@ -0,0 +1,123 @@
+from time import sleep
+
+import requests
+from lxml import html
+# import CSV
+import datetime
+
+url = 'https://m.csun.edu/default/find_parking/index'
+spacesAvailable = {"B3": None, "B5": None, "G3": None}
+spacesTaken = {"B3": None, "B5": None, "G3": None}
+
+
+def main():
+    # year, month, day, hour, minute, second
+    # waitUntilTime(2018, 10, 17, 14, 0, 0)
+    futureTime = datetime.datetime.now() + datetime.timedelta(minutes=30)
+    waitUntilTime2(futureTime)
+
+    # WORKING ON FIXING THE DAMN PROBLEM WITH THE TIME ROUNDUP TINHG FUCK
+
+    # for i in range(4):
+    #     getLotNumberCount()
+    #     CSV.writeRow(spacesAvailable['B3'], spacesAvailable['B5'], spacesAvailable['G3'])
+    #     print("Written to " + CSV.filename + " B3: " + str(spacesAvailable['B3']) + " B5: " + str(spacesAvailable['B5']) + " G3: " + str(spacesAvailable['G3']))
+    #     sleep(60 * 30)
+
+
+def getLotNumberCount():
+    B3max = 2063
+    B5max = 1361
+    G3max = 979
+    response = requests.get(url)
+    page = html.fromstring(response.content)
+
+    # Gets HTML value for the spaces
+    B3 = page.xpath('//*[@id="kgoui_Rcontent_I0_Ritems_I0_Fcolumn1"]/a/div/div/span/text()')
+    B5 = page.xpath('//*[@id="kgoui_Rcontent_I0_Ritems_I1_Fcolumn1"]/a/div/div/span/text()')
+    G3 = page.xpath('//*[@id="kgoui_Rcontent_I0_Ritems_I2_Fcolumn1"]/a/div/div/span/text()')
+
+    # SPACES AVAILABLE - Puts spaces available into dictionary and if they are full converts them
+    if str(B3[0]).lower() == "full":
+        spacesAvailable["B3"] = 0
+    else:
+        spacesAvailable["B3"] = B3[0]
+
+    if str(B5[0]).lower() == "full":
+        spacesAvailable["B5"] = 0
+    else:
+        spacesAvailable["B5"] = B5[0]
+
+    if str(G3[0]).lower() == "full":
+        spacesAvailable["G3"] = 0
+    else:
+        spacesAvailable["G3"] = G3[0]
+
+    # SPACES TAKEN - Puts spaces taken into dictionary and if they are full converts them
+    if str(B3[0]).lower() == "full":
+        spacesTaken["B3"] = B3max
+    else:
+        spacesTaken["B3"] = B3max - int(B3[0])
+
+    if str(B5[0]).lower() == "full":
+        spacesTaken["B5"] = B5max
+    else:
+        spacesTaken["B5"] = B5max - int(B5[0])
+
+    if str(G3[0]).lower() == "full":
+        spacesTaken["G3"] = G3max
+    else:
+        spacesTaken["G3"] = G3max - int(G3[0])
+
+#Will wait until inputted datetime from current time
+def waitUntilTime2(dt):
+    now = datetime.datetime.now()
+    now = convertTime(now)
+    print(now)
+
+    dt = convertTime(dt)
+    print(dt)
+
+
+
+def waitUntilTime(year, month, day, hour, minute, second):
+    # Gets current time and future time
+    now = datetime.datetime.now()
+    timeThen = datetime.datetime(year, month, day, hour, minute, second, 0)
+
+    # Formats time into readable format
+    nowFormatted = now.strftime('%B %d %Y %H:%M:%S')
+    timeThenFormatted = timeThen.strftime('%B %d %Y %H:%M:%S')
+
+    print("Now Waiting Until: " + timeThenFormatted + "...")
+    while (nowFormatted != timeThenFormatted):
+        nowFormatted = datetime.datetime.now().strftime('%B %d %Y %H:%M:%S')
+        print(nowFormatted)  # Can Remove
+        sleep(1)
+    print("Wait Finished")
+
+
+def convertTime(inputtedDateTime):
+    #converts time to simple time
+    #it removes seconds and microseconds
+
+    dt = inputtedDateTime
+    newNow = (dt - datetime.timedelta(microseconds=dt.microsecond) - datetime.timedelta(seconds=dt.second))
+    return newNow
+
+# #Starts at inputted datetime minus seconds and microseconds
+# def startAt(dt):
+#     now = dt
+#     newNow = (now - datetime.timedelta(microseconds=now.microsecond) - datetime.timedelta(seconds=now.second))
+#
+#     print(newNow)
+#     then = newNow + datetime.timedelta(minutes=30)
+#     print(then)
+#
+#     # futureTime = datetime.datetime(2018, 10, 17, 13, 0)
+#     # print(futureTime)
+#     # print(futureTime + datetime.timedelta(minutes=30))
+
+
+if __name__ == "__main__":
+    main()
